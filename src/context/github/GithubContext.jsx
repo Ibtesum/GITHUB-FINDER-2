d248b1d8b7c9da1127fd0ea78694f0122d6872ad
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-01-21 00:10:53 +0600

    Refactoring Done

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -4,79 +4,22 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = import.meta.env.VITE_REACT_APP_GITHUB_URL;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
     loading: false,
-    repos : []
+    repos: [],
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial results for testing
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
-
-    const { items } = await response.json();
-    // console.log(items);
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-  // Get a single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`);
-
-    if (response.status === 404) {
-      // window.location = "/notfound";
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  // Clear User
-  const clearUsers = () => {
-    dispatch({
-      type: "CLEAR_USERS",
-      payload: [],
-    });
-  };
-
-  // Set Loading function
-  const setLoading = () =>
-    dispatch({
-      type: "SET_LOADING",
-    });
-
   return (
     <GithubContext.Provider
       value={{
-        user: state.user,
-        users: state.users,
-        loading: state.loading,
-        getUser,
-        searchUsers,
-        clearUsers,
-        repos: state.repos,
-        dispatch
+        ...state,
+
+        dispatch,
       }}
     >
       {children}
--- src/context/github/GithubReducer.jsx
+++ src/context/github/GithubReducer.jsx
@@ -28,7 +28,7 @@ const githubReducer = (state, action) => {
     case "CLEAR_USERS":
       return {
         ...state,
-        users: action.payload,
+        users:[],
       };
 
     default:
